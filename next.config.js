bf8389133ad0221b14100d969ed15c18fefbe6d2
julakshah <julakshah@gmail.com>
2025-09-02 18:22:30 -0400

    node workflow for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
+/*http://localhost:3000* @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  output: "export",  // <=== enables static exports reactStrictMode: true,
+};
+
+module.exports = nextConfig;
