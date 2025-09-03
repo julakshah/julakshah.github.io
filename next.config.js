4691951fc21137ea8686fbf9478c4d28cb17cf81
julakshah <julakshah@gmail.com>
2025-09-02 21:06:57 -0400

    static version of website

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-/*http://localhost:3000* @type {import('next').NextConfig} */
-const nextConfig = {}
-
 const nextConfig = {
   output: "export",  // <=== enables static exports reactStrictMode: true,
 };
