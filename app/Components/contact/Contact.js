c5def313050036ddad2d8d252b791e1e0ca35122
julakshah <julakshah@gmail.com>
2025-10-29 13:42:17 -0400

    updating readme

--- app/Components/contact/Contact.js
+++ app/Components/contact/Contact.js
@@ -4,7 +4,7 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <section className='contact container section' id='contact'>
-      <h2 className='section__title'>Get In Touch</h2>
+      <h2 className='section__title'>Get In Touch!</h2>
       <div className='contact__container grid'>
         <div className='contact__info'>
           <h3 className='contact__title'> Lets Talk!</h3>
