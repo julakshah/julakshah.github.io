f5223a80ebb9a13ba663df4e4ff5dbacb48172c9
julakshah <julakshah@gmail.com>
2025-11-12 15:49:19 -0500

    trying to get turbo to load mpx webpack

--- next.config.js
+++ next.config.js
@@ -2,12 +2,10 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const withMDX = createMDX({
-  // Optional: enable `.mdx` extension support
   extension: /\.mdx?$/,
 });
 
 const nextConfig = {
-  // Add the MDX extension to page routing
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 
   images: {
