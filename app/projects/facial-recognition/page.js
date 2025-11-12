188beac4528c380b3fb34aacf5a6b372e5e1afbd
julakshah <julakshah@gmail.com>
2025-11-12 15:32:31 -0500

    renaming to mdx for turbo to read

--- app/projects/facial-recognition/page.js
+++ app/projects/facial-recognition/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Post from "./post.md";
+import Post from "./post.mdx";
 import Sidebar from "../../Components/sidebar/Sidebar";
 
 export default function FacialRecognition() {
